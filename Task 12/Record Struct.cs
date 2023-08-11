9f0abfd41b89adfb1c436b7c3223ae70bd2ab404
yamini1611 <yaminipriya1611@gmail.com>
2023-08-11 17:01:58 +0530

    Add project files.

---
+++ Task 12/Record Struct.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace System.Runtime.CompilerServices
+{
+    public static class IsExternalInit
+    {
+        public record Employee(string Name, double Salary, double Bonus)
+        {
+            public Employee UpgradedSalary(double Updatedsalary) => this with { Salary = Updatedsalary };
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Salary");
+            double Salary = double.Parse(Console.ReadLine());
+
+            double Bonus;
+
+            switch (Salary)
+            {
+                case <=50000 :
+                    Bonus = 4500;
+                    break;
+                case <=75000:
+                    Bonus = 6000;
+                    break;
+                case  90000:
+                    Bonus = 8000;
+                    break;
+                default:
+                    Bonus = 3000;
+                    break;
+            }
+
+            Employee employee = new Employee(Name, Salary, Bonus);
+            Console.WriteLine($"Employee {employee.Name} was receiving Salary of {employee.Salary} ");
+            Console.WriteLine($"Employee {employee.Name} will be receiving Salary of {employee.Salary + Bonus}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task 12/hierarchical.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HierarchicalInheritanceExample
+{
+    public record Langknown(List<string> Strings)
+    {
+        public void DisplayFullName()
+        {
+            Console.WriteLine("<---------------------->");
+            Console.WriteLine("Languages Known");
+            Console.WriteLine("<---------------------->");
+
+            for (int i = 0; i < Strings.Count;i++)
+            {
+                Console.WriteLine(Strings[i]);
+            }
+
+        }
+    }
+
+    public record LangNot(List<string> Strings, List<string> Not) :Langknown( Strings)
+    {
+        public void DisplayStudentInfo()
+        {
+            DisplayFullName();
+            Console.WriteLine("<---------------------->");
+            Console.WriteLine("Languages Not Known");
+            Console.WriteLine("<---------------------->");
+            for (int i = 0; i < Not.Count;i++)
+            {
+                Console.WriteLine(Strings[i]);
+            }
+        }
+    }
+
+    public record TechHalf(List<string> Strings, List<string> Half) : Langknown(Strings)
+    {
+        public void DisplayTeacherInfo()
+        {
+            DisplayFullName();
+            Console.WriteLine("<---------------------->");
+            Console.WriteLine("Languages Slightly Known");
+            Console.WriteLine("<---------------------->");
+            for (int i=0;i<Half.Count;i++)
+            {
+                Console.WriteLine(Half[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Strings = new List<string>();
+            Langknown known = new Langknown(Strings);
+            Strings.Add("HTML");
+            Strings.Add("CSS");
+            Strings.Add("JavaSript");
+            Strings.Add("TypeScript");
+            Strings.Add("React");
+            Strings.Add("SQL Server");
+            Strings.Add("C#");
+          
+
+            List<string> Not = new List<string>();
+            LangNot notknown = new LangNot(Strings, Not);
+            Not.Add("Django");
+            Not.Add("Node ");
+            Not.Add("Ruby");
+            Not.Add("Angular");
+            Not.Add("Vue JS");
+            Not.Add("PHP");
+           
+
+            notknown.DisplayStudentInfo();
+           
+            List<string> Half = new List<string>();
+            TechHalf tech = new TechHalf(Strings ,Half);
+            Half.Add("C");
+            Half.Add("C++");
+            Half.Add("JAVA");
+            Half.Add("Jquery");
+            tech.DisplayTeacherInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
